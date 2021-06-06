03b8beabd5ff0413e1aab76879bce0d5c52f3812
Mounia-Kacemi <niaamo12@gmail.com>
2021-06-06 11:26:29 +0100

    my commit

---
+++ src/main/java/projet/bdcc/CinemaProjetApplication.java
@@ -0,0 +1,37 @@
+package projet.bdcc;
+
+
+import projet.bdcc.entities.Film;
+
+import projet.bdcc.entities.Salle;
+import projet.bdcc.entities.Ticket;
+import projet.bdcc.service.ICinemaInitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class CinemaProjetApplication implements CommandLineRunner {
+	@Autowired private ICinemaInitService cinemaInitService;
+	@Autowired private RepositoryRestConfiguration restConfiguration;
+	public static void main(String[] args) {
+	SpringApplication.run(CinemaProjetApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+	restConfiguration.exposeIdsFor(Film.class, Salle.class, Ticket.class);
+	cinemaInitService.initVilles(); 
+	cinemaInitService.initCinemas();
+	cinemaInitService.initSalles(); 
+	cinemaInitService.initPalces();
+	cinemaInitService.initSeances(); 
+	cinemaInitService.initCategories();
+	cinemaInitService.initFilms();
+	cinemaInitService.initProjections();
+	cinemaInitService.initTickets();
+	}
+
+
+}
---
+++ src/main/java/projet/bdcc/entities/ProjectionProj.java
@@ -0,0 +1,18 @@
+package projet.bdcc.entities;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name="p1",types={projet.bdcc.entities.Projection.class})
+public interface ProjectionProj {
+	
+	public Long getId();
+	public double getPrix();
+	public Date getDateProjection();
+	public Salle getSalle();
+	public Film getFilm();
+	public Sceance getSceance();
+	public Collection<Ticket> getTickets();
+}
